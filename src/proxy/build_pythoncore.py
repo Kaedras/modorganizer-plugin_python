026ce57ab1f4ade97a91f1b1f8dfdd8f4faf8c2b
Kaedras <6783810+Kaedras@users.noreply.github.com>
2026-03-15 23:17:54 +0100

    Fix `build_pythoncore.py` on linux

--- src/proxy/build_pythoncore.py
+++ src/proxy/build_pythoncore.py
@@ -3,8 +3,10 @@ import zipfile
 from pathlib import Path
 
 _EXCLUDE_MODULES = ["ensurepip", "idlelib", "test", "tkinter", "turtle_demo", "venv"]
-
-libdir = Path(sys.executable).parent.joinpath("Lib")
+if sys.platform == "win32":
+    libdir = Path(sys.executable).parent.joinpath("Lib")
+else:
+    libdir = Path("/lib/python%d.%d" % (sys.version_info.major, sys.version_info.minor))
 assert libdir.exists()
 
 with zipfile.PyZipFile(sys.argv[1], optimize=2, mode="w") as fp:
